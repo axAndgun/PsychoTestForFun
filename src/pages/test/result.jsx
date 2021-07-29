018c23c748390ff542b6265fb555506227c497a3
gundopark-123 <87962537+axAndgun@users.noreply.github.com>
2021-07-29 22:46:05 +0900

    build-error-fixed

--- src/pages/test/result.jsx
+++ src/pages/test/result.jsx
@@ -1,11 +1,10 @@
 import { GlobalContext } from '../_app'
-import { useContext,useState } from 'react'
+import { useContext } from 'react'
 import useSwr from 'swr'
 import {fetcher} from '../../utils/commons'
 import Image from 'next/image'
-import {focusInExpandFwd} from '../../styles/SpecialEffect'
 import styled from 'styled-components'
-import ClientSideLink from '../../components/atoms/ClientSideLink'
+//import { test } from '../../models/BinaryQuestionResults'
 
 const LocationText = styled.h3`
     margin: 1rem;
@@ -20,15 +19,29 @@ const ResultTag = styled(LocationText)`
 
 
 function TestResult() {
- const { data: test, error } = useSwr(`/api/test/`, fetcher)
-//const { data: test, error } = useSwr(`/api/result/`, fetcher)
+ const { data: test, error } = useSwr(`/api/test/`, fetcher) //빌드시 undefined 에러 발생!!! 
+//const { data: test, error } = useSwr(`/api/result/`, fetcher) 
+
+const isTestLoading = !test && ! error 
+
+if (isTestLoading){  // 이 코드를 추가하니 undefined 에러가 해결되었다. 
+    return (
+        <>
+        로딩중...
+        </>
+    )
+}
+
+
+
+
 
  const { values }  = useContext(GlobalContext)
 //const resultIndex = test.results.findIndex((result)=>result.condition(values))
 //const result = test.results[resultIndex] ?? test.results[0]
  
- const results = test.results
- //const [result,setResult] = useState(results[0]) //too much re-render error occurred.
+ const results = test.results //빌드시 undefined 에러 발생!!! (해결책 line num: 27 --> isTestLoading)
+ 
  let result =results[0]// let 재할당
 
  if(values>=60){
